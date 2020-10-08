9477547dbc47f4482c1e0fe65824d9a10f983fa1
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-08 19:09:06 -0400

    Now return a valid jwt token

--- TbspApi/Services/UserService.cs
+++ TbspApi/Services/UserService.cs
@@ -1,6 +1,11 @@
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 
 using TbspApi.Entities;
 using TbspApi.Models;
@@ -32,14 +37,28 @@ namespace TbspApi.Services {
             if (user == null) return null;
 
             // authentication successful so generate jwt token
-            //var token = generateJwtToken(user);
-            var token = _jwtSettings.secret;
+            var token = generateJwtToken(user);
 
             return new AuthenticateResponse(user, token);
         }
 
         public IEnumerable<User> GetAll() {
             return _users;
+        }
+
+        private string generateJwtToken(User user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
