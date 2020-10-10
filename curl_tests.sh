e6d387766265dae5e6ef3a1c80bc1a3b625c5db4
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-10 13:59:16 -0400

    secret should now be set in an environment variable

--- curl_tests.sh
+++ curl_tests.sh
@@ -1,3 +1,4 @@
 curl -v -X POST -H "Content-Type: application/json" -d '{"Username": "test", "Password": "test"}'  http://localhost:8000/users/authenticate
 
 curl -v -X GET http://localhost:8000/users
+curl -v -X GET -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJuYmYiOjE2MDIyMDEyMzgsImV4cCI6MTYwMjgwNjAzOCwiaWF0IjoxNjAyMjAxMjM4fQ.GIchynzZ_Eq8SCcosESEkwNUspzTM-alfEn6YbuNn58" http://localhost:8000/users
