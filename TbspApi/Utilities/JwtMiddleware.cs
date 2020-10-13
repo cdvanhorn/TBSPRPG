2b8bd1fb948372d96097ac654bab56c602dbf066
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-12 20:31:07 -0400

    removed unnecessary using statements

--- TbspApi/Utilities/JwtMiddleware.cs
+++ TbspApi/Utilities/JwtMiddleware.cs
@@ -1,10 +1,5 @@
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using TbspApi.Services;
 
