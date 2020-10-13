5faae6025578e1e2517ad3841a7a7f4c57cb7c7e
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-12 20:29:45 -0400

    moved jwt token validation to the helper

--- TbspApi/Utilities/JwtHelper.cs
+++ TbspApi/Utilities/JwtHelper.cs
@@ -2,6 +2,7 @@ using Microsoft.IdentityModel.Tokens;
 
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 
@@ -10,6 +11,7 @@ using TbspApi.Entities;
 namespace TbspApi.Utilities {
     public interface IJwtHelper {
         string GenerateToken(User user);
+        string ValidateToken(string token);
     }
 
     public class JwtHelper : IJwtHelper{
@@ -18,6 +20,22 @@ namespace TbspApi.Utilities {
         public JwtHelper(IJwtSettings jwtSettings) {
             _jwtSettings = jwtSettings;
             _tokenHandler = new JwtSecurityTokenHandler();
+        }
+
+        public string ValidateToken(string token) {
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            _tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                ClockSkew = TimeSpan.Zero
+            }, out SecurityToken validatedToken);
+
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            return jwtToken.Claims.First(x => x.Type == "id").Value;
         }
 
         public string GenerateToken(User user) {
--- TbspApi/Utilities/JwtMiddleware.cs
+++ TbspApi/Utilities/JwtMiddleware.cs
@@ -21,35 +21,21 @@ namespace TbspApi.Utilities
             _jwtSettings = jwtSettings;
         }
 
-        public async Task Invoke(HttpContext context, IUserService userService)
+        public async Task Invoke(HttpContext context, IUserService userService, IJwtHelper jwtHelper)
         {
             var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
 
             if (token != null)
-                await attachUserToContext(context, userService, token);
+                await attachUserToContext(context, userService, jwtHelper, token);
 
             await _next(context);
         }
 
-        private async Task attachUserToContext(HttpContext context, IUserService userService, string token)
+        private async Task attachUserToContext(HttpContext context, IUserService userService, IJwtHelper jwtHelper, string token)
         {
             try
             {
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
-                tokenHandler.ValidateToken(token, new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
-                    ClockSkew = TimeSpan.Zero
-                }, out SecurityToken validatedToken);
-
-                var jwtToken = (JwtSecurityToken)validatedToken;
-                var userId = jwtToken.Claims.First(x => x.Type == "id").Value;
-
+                var userId = jwtHelper.ValidateToken(token);
                 // attach user to context on successful jwt validation
                 context.Items["User"] = await userService.GetById(userId);
             }
