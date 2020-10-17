c76802c8febe807449617860eaa179db306b0701
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-17 10:13:51 -0400

    removed some notes

--- frontend/src/app/services/content.service.ts
+++ frontend/src/app/services/content.service.ts
@@ -6,12 +6,4 @@ import { Injectable } from '@angular/core';
 export class ContentService {
 
   constructor() { }
-
-  getContents() {
-    //gets all content from this session, content gets cleared when session ended
-  }
-
-  getLatestContent() {
-    //gets the text content from the latest update
-  }
 }
--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -6,18 +6,4 @@ import { Injectable } from '@angular/core';
 export class GameService {
 
   constructor() { }
-
-  updateState() {
-    //or we make an update call and call gets on things that could be affected, it's probably
-    //the most modulear
-
-    //ok this will do a post which will update all of the status of the game on the backend
-
-    //{game: "demo", command: "Pickup Banana"}
-  }
-
-  getGames() {
-    //gets a list of available tbsp games, for now it will just be the demo, this won't be
-    //used in game it will be used in the outer interface
-  }
 }
