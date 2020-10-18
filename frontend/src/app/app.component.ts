65f5847edd31553d50fb9f673a298bb95533b348
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-18 13:04:20 -0400

    cleaning up the frontend, can start frontend with api layer via docker

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tbsprpg (text based single player rpg)';
   version = '0.01';
+  //going to connect to the api to get a list of adventures
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,23 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
-import { TextwindowComponent } from './textwindow/textwindow.component';
-import { VerbsComponent } from './verbs/verbs.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { CharacterComponent } from './character/character.component';
-import { InventoryComponent } from './inventory/inventory.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent,
-    TextwindowComponent,
-    VerbsComponent,
-    NavigationComponent,
-    CharacterComponent,
-    InventoryComponent
+    GameComponent
   ],
   imports: [
     BrowserModule,
