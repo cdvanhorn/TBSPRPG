358a8113a6fd3bc8cbdec5dd9b53aad62359e2de
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-18 12:33:14 -0400

    exposed the adventures api

---
+++ TbspApi/Controllers/AdventuresController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+using RestSharp;
+
+using System.Threading.Tasks;
+
+using TbspApi.Models;
+using TbspApi.Services;
+
+namespace TbspApi.Controllers {
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class AdventuresController : ControllerBase {
+
+        public AdventuresController() {
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var client = new RestClient($"http://adventureapi:8001/adventures");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = await client.ExecuteAsync(request);
+
+            return Ok(response.Content);
+            //return Ok(null);
+        }
+    }
+}
