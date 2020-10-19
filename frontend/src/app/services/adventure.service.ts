fd8786e7dd76773f2241c04bc02d27acf107d393
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-19 19:56:31 -0400

    can now connect to a mock api using a json-server container

--- frontend/src/app/services/adventure.service.ts
+++ frontend/src/app/services/adventure.service.ts
@@ -27,9 +27,6 @@ export class AdventureService {
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
   
-      // TODO: better job of transforming error for user consumption
-      //this.log(`${operation} failed: ${error.message}`);
-  
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
