39fe988a8a977b52b1af64f4ae6f38b9aaafc5b6
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-20 18:09:08 -0400

    working on the ui, added routing, started console component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConsoleComponent } from './components/console/console.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'console', component: ConsoleComponent },
+  { path: '', redirectTo: '/console', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AdventuresComponent } from './components/adventures/adventures.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ConsoleComponent } from './components/console/console.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    AdventuresComponent
+    AdventuresComponent,
+    ConsoleComponent
   ],
   imports: [
     BrowserModule,
