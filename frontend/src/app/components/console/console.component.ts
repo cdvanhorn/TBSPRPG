fe258a45c69d94a2eb371d37ea99855166ddd2e3
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-10-20 18:54:32 -0400

    console fake echo
    I have a console component that can fake an echo command.

--- frontend/src/app/components/console/console.component.ts
+++ frontend/src/app/components/console/console.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-console',
   templateUrl: './console.component.html',
   styleUrls: ['./console.component.scss']
 })
 export class ConsoleComponent implements OnInit {
+  prompt: string = '>';
+  command: string;
+  output : string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleSubmit(e){
+    e.preventDefault();
+    this.output = this.command.slice(5);
+    //console.log(this.command);
+  }
+
+  handleKeyUp(e) {
+    if(e.keyCode === 13) {
+      this.handleSubmit(e);
+    }
+  }
 }
