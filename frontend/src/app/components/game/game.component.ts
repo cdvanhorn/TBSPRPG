42ca52ec839e014a214e81d8768ce8c7b09f2cee
cdvanhorn <34102449+cdvanhorn@users.noreply.github.com>
2020-10-26 18:33:31 -0400

    Css grid (#1)
    * UI Skeleton
    
    css grid is so much easier for me than flex box
    
    * UI Skeleton
    
    switched verbs to css grid
    
    * UI Skeleton
    
    everything is using css grid

--- frontend/src/app/components/game/game.component.ts
+++ frontend/src/app/components/game/game.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GameLayout } from './models/GameLayout';
 
 @Component({
   selector: 'app-game',
@@ -9,20 +8,10 @@ import { GameLayout } from './models/GameLayout';
 })
 export class GameComponent implements OnInit {
   adventure: string;
-  windowWidth: number;
-  windowHeight: number;
-  layout: GameLayout;
 
-  constructor(private route: ActivatedRoute) {
-    this.layout = new GameLayout();
-  }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    //figure out the coordinates for everything
-    this.windowWidth = window.innerWidth;
-    this.windowHeight = window.innerHeight;
-    this.layout.updateValues(this.windowWidth, this.windowHeight);
-
     this.route.params.subscribe( params => {
       this.adventure = params['adventure'];
     });
@@ -40,59 +29,5 @@ export class GameComponent implements OnInit {
     //otherwise post a new game for this adventure
 
     //for this skeleton we're going to assume they've started the game already
-  }
-
-  computeContentStyle(): any {
-    //outline: 1px solid blue;position: absolute; top: 0; left: 0; width: {{windowWidth}}px; height: 616px;
-    var style = { 
-      'position': 'absolute',
-      'top': this.layout.contentY + 'px',
-      'left': this.layout.contentX + 'px',
-      'width': this.layout.contentWidth + 'px',
-      'height': this.layout.contentHeight + 'px',
-      'overflow-y':'scroll'
-    };
-    return style;
-  }
-
-  computeMovementStyle(): any {
-    var style = { 
-      'position': 'absolute',
-      'top': this.layout.movementY + 'px',
-      'left': this.layout.movementX + 'px',
-      'width': this.layout.movementWidth + 'px',
-      'height': this.layout.movementHeight + 'px'
-    };
-    return style;
-  }
-
-  computeVerbStyle(): any {
-    var style = { 
-      'position': 'absolute',
-      'top': this.layout.verbsY + 'px',
-      'left': this.layout.verbsX + 'px',
-      'width': this.layout.verbsWidth + 'px',
-      'height': this.layout.verbsHeight + 'px'
-    };
-    return style;
-  }
-
-  computeInventoryStyle(): any {
-    var style = { 
-      'position': 'absolute',
-      'top': this.layout.inventoryY + 'px',
-      'left': this.layout.inventoryX + 'px',
-      'width': this.layout.inventoryWidth + 'px',
-      'height': this.layout.inventoryHeight + 'px',
-      'overflow-y':'scroll'
-    };
-    return style;
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.windowWidth = window.innerWidth;
-    this.windowHeight = window.innerHeight;
-    this.layout.updateValues(this.windowWidth, this.windowHeight);
   }
 }
