4c2f17452d0f6e8eefebf060e5dcba4c0ed696c1
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-11-02 17:51:53 -0500

    User Login
    Fixed bad include.

--- frontend/src/app/components/game/game.component.ts
+++ frontend/src/app/components/game/game.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Console } from 'console';
 
 @Component({
   selector: 'app-game',
