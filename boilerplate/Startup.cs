ff9a2186ae35a99cfbf33264b420f77d0c0d4995
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-12-07 21:33:28 -0500

    MapAPI
    Documenting how to create a new service.

---
+++ boilerplate/Startup.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using TbspRpgLib.Jwt;
+using TbspRpgLib.Settings;
+using TbspRpgLib.Events;
+
+namespace GameApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddScoped<IEventAdapter, EventAdapter>();
+            services.AddScoped<IEventService, EventService>();
+
+            services.Configure<DatabaseSettings>(Configuration.GetSection("Database"));
+            services.AddSingleton<IDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+                
+            services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
+            services.AddSingleton<IJwtSettings>(sp =>
+                sp.GetRequiredService<IOptions<JwtSettings>>().Value);
+            
+            services.Configure<EventStoreSettings>(Configuration.GetSection("EventStore"));
+            services.AddSingleton<IEventStoreSettings>(sp =>
+                sp.GetRequiredService<IOptions<EventStoreSettings>>().Value);
+
+            //start workers
+            //services.AddHostedService<MyNewGameEventProcessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseMiddleware<JwtMiddleware>();
+
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
