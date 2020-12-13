2ab4129d7e37bf222aa0c734d3083a8a00cb0235
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-12-13 16:22:03 -0500

    MapAPI
    Starting unit tests for shared library.

---
+++ TbspRpgLib.Tests/Aggregate/AggregateServiceTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Moq;
+
+using Xunit;
+
+using TbspRpgLib.Aggregates;
+using TbspRpgLib.Events;
+using TbspRpgLib.Events.Content;
+
+namespace TbspRpgLib.Tests.Aggregate {
+    public class AggregateServiceTests {
+        private AggregateService aggregateService;
+
+        public AggregateServiceTests() {
+            List<Event> events = new List<Event>();
+            events.Add(
+                new NewGameEvent(
+                    new NewGame {
+                        Id = "1",
+                        UserId = "1",
+                        AdventureName = "Demo",
+                        AdventureId = "1"
+                    }
+                )
+            );
+            //we need to mock _eventService.GetEventsInStreamAsync for BuildAggregate
+            //it needs to return a list of event like objects
+            var mockEventService = new Mock<IEventService>();
+            mockEventService.Setup(service => 
+                service.GetEventsInStreamAsync(It.IsAny<string>())
+            ).ReturnsAsync(
+                (string eventid) =>
+                    events.Where(evnt => evnt.GetStreamId() == eventid).ToList()
+            );
+
+            aggregateService = new AggregateService(mockEventService.Object);
+        }
+
+        [Fact]
+        public async void BuildAggregate_NewGameEvent_IsValid() {
+            //arrange
+            //act
+            var aggregate = await aggregateService.BuildAggregate("1", "GameAggregate");
+            var game = (GameAggregate)aggregate;
+            //assert
+            Assert.Equal("1", game.Id);
+            Assert.Equal("Demo", game.AdventureName);
+        }
+    }
+}
