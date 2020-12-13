9e875d9ea1ed9cd9bfaa5a0dd9565b750bea4835
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-12-13 17:07:18 -0500

    MapAPI
    Pulled the event handler code in the aggregate service in to it's own function.

--- TbspRpgLib/Aggregates/AggregateService.cs
+++ TbspRpgLib/Aggregates/AggregateService.cs
@@ -19,21 +19,25 @@ namespace TbspRpgLib.Aggregates {
         public void SubscribeByType(string typeName, Action<Aggregate, string, ulong> eventHandler, string servicePrefix, ulong subscriptionStart = 0) {
             _eventService.SubscribeByType(
                 typeName,
-                async (evnt) => {
-                    //check if the aggregate id is ok, produce an aggregate
-                    var aggregateId = evnt.GetStreamId();
-                    if(aggregateId == null) //we can't parse this event
-                        return;
-
-                    //check if the event id prefixed with the service id exists in the list of processed ids in the aggregrate
-                    var aggregate = await BuildAggregate(aggregateId, "GameAggregate");
-                    //only call the event handler if we haven't processed this event
-                    var processedId = $"{servicePrefix}_{evnt.EventId}";
-                    if(!aggregate.ProcessedEventIds.Contains(processedId))
-                        eventHandler(aggregate, evnt.EventId.ToString(), evnt.Position);
+                (evnt) => {
+                    HandleEvent(evnt, eventHandler, servicePrefix);
                 },
                 subscriptionStart
             );
+        }
+
+        public async void HandleEvent(Event evnt, Action<Aggregate, string, ulong> eventHandler, string servicePrefix) {
+            //check if the aggregate id is ok, produce an aggregate
+            var aggregateId = evnt.GetStreamId();
+            if(aggregateId == null) //we can't parse this event
+                return;
+
+            //check if the event id prefixed with the service id exists in the list of processed ids in the aggregrate
+            var aggregate = await BuildAggregate(aggregateId, "GameAggregate");
+            //only call the event handler if we haven't processed this event
+            var processedId = $"{servicePrefix}_{evnt.EventId}";
+            if(!aggregate.ProcessedEventIds.Contains(processedId))
+                eventHandler(aggregate, evnt.EventId.ToString(), evnt.Position);
         }
 
         public async Task<Aggregate> BuildAggregate(string aggregateId, string aggregateTypeName) {
