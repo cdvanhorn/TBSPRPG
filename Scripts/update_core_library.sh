6368b6bf67b6183daec81d6942575088f0d29ec0
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-01-27 19:16:11 -0500

    GameSystemAPI
    Updated notes and boilerplate files.

--- Scripts/update_core_library.sh
+++ Scripts/update_core_library.sh
@@ -5,11 +5,13 @@ docker ps -a -q --filter ancestor=tbsprpg_gameapi:latest | xargs docker rm
 docker ps -a -q --filter ancestor=tbsprpg_mapapi:latest | xargs docker rm
 docker ps -a -q --filter ancestor=tbsprpg_adventureapi:latest | xargs docker rm
 docker ps -a -q --filter ancestor=tbsprpg_userapi:latest | xargs docker rm
+docker ps -a -q --filter ancestor=tbsprpg_gamesystemapi:latest | xargs docker rm
 docker image rm tbsprpg_publicapi:latest \
 	tbsprpg_gameapi:latest \
 	tbsprpg_mapapi:latest \
 	tbsprpg_adventureapi:latest \
-	tbsprpg_userapi:latest
+	tbsprpg_userapi:latest \
+	tbsprpg_gamesystemapi:latest
 
 #build a new version of the library
 cd ../TbspRpgLib
@@ -22,3 +24,4 @@ cd ../Scripts
 ./link_library.sh ../../TBSPRPG_MapAPI
 ./link_library.sh ../../TBSPRPG_AdventureAPI
 ./link_library.sh ../../TBSPRPG_UserAPI
+./link_library.sh ../../TBSPRPG_GameSystemAPI
