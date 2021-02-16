6d76b29416a958ff7f1172f70c3046487d1a62c6
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-02-15 22:05:52 -0500

    TbspRpgLib
    Further improved script

--- Scripts/update_core_library.sh
+++ Scripts/update_core_library.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-docker ps -a -q --filter ancestor=tbsprpg_publicapi:latest | xargs docker rm
-docker ps -a -q --filter ancestor=tbsprpg_gameapi:latest | xargs docker rm
-docker ps -a -q --filter ancestor=tbsprpg_mapapi:latest | xargs docker rm
-docker ps -a -q --filter ancestor=tbsprpg_adventureapi:latest | xargs docker rm
-docker ps -a -q --filter ancestor=tbsprpg_userapi:latest | xargs docker rm
-docker ps -a -q --filter ancestor=tbsprpg_gamesystemapi:latest | xargs docker rm
-docker image rm tbsprpg_publicapi:latest \
-	tbsprpg_gameapi:latest \
-	tbsprpg_mapapi:latest \
-	tbsprpg_adventureapi:latest \
-	tbsprpg_userapi:latest \
-	tbsprpg_gamesystemapi:latest \
-	tbsprpg_contentapi:latest
+#docker ps -a -q --filter ancestor=tbsprpg_publicapi:latest | xargs docker rm
+#docker ps -a -q --filter ancestor=tbsprpg_gameapi:latest | xargs docker rm
+#docker ps -a -q --filter ancestor=tbsprpg_mapapi:latest | xargs docker rm
+#docker ps -a -q --filter ancestor=tbsprpg_adventureapi:latest | xargs docker rm
+#docker ps -a -q --filter ancestor=tbsprpg_userapi:latest | xargs docker rm
+#docker ps -a -q --filter ancestor=tbsprpg_gamesystemapi:latest | xargs docker rm
+#docker image rm tbsprpg_publicapi:latest \
+#	tbsprpg_gameapi:latest \
+#	tbsprpg_mapapi:latest \
+#	tbsprpg_adventureapi:latest \
+#	tbsprpg_userapi:latest \
+#	tbsprpg_gamesystemapi:latest \
+#	tbsprpg_contentapi:latest
+
+#delete images
+docker-compose down --rmi local
 
 #build a new version of the library
-cd ../TbspRpgLib
+cd ./TBSPRPG/TbspRpgLib
 dotnet pack --configuration Release
 
 #copy the libraries to the individual projects
