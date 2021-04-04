a6e068149448212ef632a4e24c72adb6c13b85ec
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-04-04 11:30:45 -0400

    TbspRpg Lib
    Allow negative starts, so can filter from the end of the stream.

--- TbspRpgLib/InterServiceCommunication/RequestModels/ContentFilterRequest.cs
+++ TbspRpgLib/InterServiceCommunication/RequestModels/ContentFilterRequest.cs
@@ -1,7 +1,7 @@
 namespace TbspRpgLib.InterServiceCommunication.RequestModels {
     public class ContentFilterRequest : ContentRequest {
         public string Direction { get; set; }
-        public ulong? Start { get; set; }
+        public long? Start { get; set; }
         public long? Count { get; set; }
     }
 }
