c87fa699ddab74ca08d2637e58c5b7f028fef34b
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-05-31 15:39:16 -0400

    TbspRpgLib
    Refactoring.

--- TbspRpgLib/InterServiceCommunication/RequestModels/Request.cs
+++ TbspRpgLib/InterServiceCommunication/RequestModels/Request.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace TbspRpgLib.InterServiceCommunication.RequestModels
 {
     public class Request
     {
-        
+        public string ServiceName { get; set; }
+        public string EndPoint { get; set; }
+        public string Token { get; set; }
+        public object Parameters { get; set; }
+        public object PostData { get; set; }
     }
 }
