9c5a89df04f94d4ff3822c552b3036a2a9afda3f
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-06-19 22:26:35 -0400

    TbspRpgLib
    Updated the db clear script.

--- Scripts/clear_db_events.sh
+++ Scripts/clear_db_events.sh
@@ -5,6 +5,8 @@ psql -U postgres -h localhost -d tbsprpg_game -c "DELETE FROM processed_events"
 psql -U postgres -h localhost -d tbsprpg_game -c "DELETE FROM event_type_positions"
 
 psql -U postgres -h localhost -d tbsprpg_map -c "DELETE FROM games"
+psql -U postgres -h localhost -d tbsprpg_map -c "DELETE FROM locations"
+psql -U postgres -h localhost -d tbsprpg_map -c "DELETE FROM routes"
 psql -U postgres -h localhost -d tbsprpg_map -c "DELETE FROM processed_events"
 psql -U postgres -h localhost -d tbsprpg_map -c "DELETE FROM event_type_positions"
 
@@ -12,5 +14,6 @@ psql -U postgres -h localhost -d tbsprpg_gamesystem -c "DELETE FROM processed_ev
 psql -U postgres -h localhost -d tbsprpg_gamesystem -c "DELETE FROM event_type_positions"
 
 psql -U postgres -h localhost -d tbsprpg_content -c "DELETE FROM contents"
+psql -U postgres -h localhost -d tbsprpg_content -c "DELETE FROM games"
 psql -U postgres -h localhost -d tbsprpg_content -c "DELETE FROM event_type_positions"
 psql -U postgres -h localhost -d tbsprpg_content -c "DELETE FROM processed_events"
